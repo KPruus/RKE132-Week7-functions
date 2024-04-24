3572697d5d066289761210ca2725f7b2d1d8d118
KPruus <kristjanpruus@hotmail.com>
2024-04-24 17:19:12 +0300

    add week7 code

---
+++ Functions/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("Are you coming or leaving? (in/out):");
+string userchoice = Console.ReadLine();
+
+if (userchoice == "in")
+{
+    PrintHello();
+}
+else if (userchoice == "out")
+{
+    PrintGoodbye();
+}
+
+
+//PrintHello();
+//PrintGoodbye();
+
+
+static void PrintHello()
+{
+    Console.WriteLine("Wello, Horld");
+}
+
+static void PrintGoodbye()
+{
+    Console.WriteLine("In a while, crocodile");
+}
---
+++ TempCalc/Program.cs
@@ -0,0 +1,37 @@
+﻿//küsi kas teisendada celsius v fahrenheit
+//küsi väärtust
+//teisenda
+//kuva tulemus
+
+
+Console.WriteLine("Enter Temperature Unit to convert (C/F)");
+char tempUnit = Char.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter the Temperature (value):");
+int tempValue = Int32.Parse(Console.ReadLine());
+
+
+switch (tempUnit)
+{
+    case 'C':
+        ConvertToFahrenheit(tempValue);
+        break;
+    case 'F':
+        ConvertToCelsius(tempValue);
+        break;
+    default:
+        Console.WriteLine("bad input :(");
+        Console.WriteLine("(NB: Make sure you're using UPPERCASE letters for entering units)");
+        break;
+}
+static void ConvertToFahrenheit(int a)
+{
+    Console.WriteLine($"{a} degrees in Celsius = ({a} * 9) / 5 + 32 = {(a * 9) / 5 + 32} degrees in Fahrenheit ");
+}
+
+static void ConvertToCelsius(int a)
+{
+    Console.WriteLine($"{a} degrees in Fahrenheit = ({a} - 32) * 5/9 = {(a - 32) * 5/9} degrees in Celsius");
+}
+
+
